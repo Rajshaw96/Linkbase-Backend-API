c3e1d939b2ee8b9f7b6e2587866fa46ea802a6e2
Rajkumar Verma <rajkumarshaw624@gmail.com>
2025-07-20 22:59:17 +0530

    Form API Update

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -24,6 +24,19 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Get form by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const form = await Form.findById(req.params.id);
+    if (!form) {
+      return res.status(404).json({ error: "Form not found" });
+    }
+    res.json(form);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
 router.put("/:id", async (req, res) => {
   try {
     const updatedForm = await Form.findByIdAndUpdate(req.params.id, req.body, {
@@ -38,39 +51,6 @@ router.put("/:id", async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 });
-
-router.get("/responses", async (req, res) => {
-  try {
-    const responses = await FormResponse.find().sort({ submittedAt: -1 });
-    res.json(responses);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-// Get form by ID
-router.get("/:id", async (req, res) => {
-  try {
-    const form = await Form.findById(req.params.id);
-    if (!form) {
-      return res.status(404).json({ error: "Form not found" });
-    }
-    res.json(form);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-router.post("/responses/", async (req, res) => {
-  try {
-    const response = new FormResponse(req.body);
-    const saved = await response.save();
-    res.status(201).json(saved);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
 
 router.delete("/delete/:id", async (req, res) => {
   try {
@@ -88,5 +68,37 @@ router.delete("/delete/:id", async (req, res) => {
     res.status(500).json({ message: "Server error while deleting form." });
   }
 });
+
+router.post("/responses", async (req, res) => {
+  try {
+    const response = new FormResponse(req.body);
+    const saved = await response.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+router.get("/responses", async (req, res) => {
+  try {
+    const responses = await FormResponse.find().sort({ submittedAt: -1 });
+    res.json(responses);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+router.get("/responses/:id", async (req, res) => {
+  try {
+    const response = await FormResponse.findById(req.params.id);
+    if (!response) {
+      return res.status(404).json({ error: "Form response not found" });
+    }
+    res.json(response);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
 
 module.exports = router;
